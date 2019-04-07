2857b464032cdd9701cdc64f41cdce423ae0241b
Osman ORHAN <osmnorhn@gmail.com>
2019-04-07 21:09:50 +0300

    AWS Lambda Sample project with .NET Core 2.1 Web API

---
+++ LambdaShoppingListWebAPI/Controllers/ShoppingListController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LambdaShoppingListWebAPI.Models;
+using LambdaShoppingListWebAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LambdaShoppingListWebAPI.Controllers
+{
+    [Route("v1/shoppingList")]
+    public class ShoppingListController : ControllerBase
+    {
+        private readonly IShoppingListService _shoppingListService;
+
+        public ShoppingListController(IShoppingListService shoppingListService)
+        {
+            _shoppingListService = shoppingListService;
+        }
+
+        [HttpGet]
+        public IActionResult GetShoppingList()
+        {
+            var result = _shoppingListService.GetItemsFromShoppingList();
+            return Ok(result);
+        }   
+
+        [HttpPost]
+        public IActionResult AddItemToShoppingList([FromBody]ShoppingListModel shoppingList)
+        {
+            _shoppingListService.AddItemToShoppingList(shoppingList);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteItemFromShoppingList([FromBody] ShoppingListModel shoppingList)
+        {
+            _shoppingListService.RemoveItemFromShoppingList(shoppingList.Name);
+            return Ok();
+        }
+    }
+}
---
+++ LambdaShoppingListWebAPI/Services/ShoppingListService.cs
@@ -0,0 +1,27 @@
+﻿using LambdaShoppingListWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LambdaShoppingListWebAPI.Services
+{
+    public class ShoppingListService : IShoppingListService
+    {
+        private readonly Dictionary<string, int> _shoppingListStorage = new Dictionary<string, int>();
+        public Dictionary<string, int> GetItemsFromShoppingList()
+        {
+            return _shoppingListStorage;
+        }
+
+        public void AddItemToShoppingList(ShoppingListModel shoppingList)
+        {
+            _shoppingListStorage.Add(shoppingList.Name, shoppingList.Quantity);
+        }
+
+        public void RemoveItemFromShoppingList(string shoppingListName)
+        {
+            _shoppingListStorage.Remove(shoppingListName);
+        }
+    }
+}
